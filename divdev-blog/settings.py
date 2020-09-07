5253ce5c4b7e9ecc58b2632475efd6ef709e7319
Divuzki <ikdivine517@gmail.com>
2020-09-07 14:16:53 +0100

    WHITE NOISE COMMIT

--- divdev-blog/settings.py
+++ divdev-blog/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -117,10 +118,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT =  os.path.join(BASE_DIR, 'static-root')
+STATIC_ROOT =  os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "staticfiles"),
-    'staticfiles/',
+    os.path.join(BASE_DIR, "static-root")
 ]
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
