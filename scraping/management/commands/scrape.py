ead536e74d5178e07f88f8b9adf7e3c77f2beb83
Divuzki <divuzki@gmail.com>
2022-03-05 18:05:38 +0100

    Removed 0 From `num_sum function in utils.py`

--- scraping/management/commands/scrape.py
+++ scraping/management/commands/scrape.py
@@ -19,63 +19,67 @@ class Command(BaseCommand):
             post = p.find("h3", {"class":"jeg_post_title"})
             url = post.find('a')['href']
             title = post.text
-            html = requests.get(url)
-            soup = BeautifulSoup(html.content, 'html.parser')
-            # post = soup.find_all("div", class_="jeg_inner_content")
-            descs = soup.find_all("div", class_="entry-header")
-            img = soup.find_all("div", class_="jeg_featured")
-            # getting the content
-            content = soup.find_all("div", class_="content-inner")
-            desc_list = []
-            img_list = []
-            unwanted_list = []
-            content_list = []
-            try:
-                for d in descs:
-                    desc_list.append(d.find('h2').text)  # getting the discription
+            qs = Post.objects.filter(title=title).first()
+            if not qs:
+                html = requests.get(url)
+                soup = BeautifulSoup(html.content, 'html.parser')
+                # post = soup.find_all("div", class_="jeg_inner_content")
+                descs = soup.find_all("div", class_="entry-header")
+                img = soup.find_all("div", class_="jeg_featured")
+                # getting the content
+                content = soup.find_all("div", class_="content-inner")
+                desc_list = []
+                img_list = []
+                unwanted_list = []
+                content_list = []
+                try:
+                    for d in descs:
+                        desc_list.append(d.find('h2').text)  # getting the discription
 
-                # Looking for post image link
-                for img_url in img:
-                    img_list.append(img_url.find('a')['href'])  # getting the image_url
+                    # Looking for post image link
+                    for img_url in img:
+                        img_list.append(img_url.find('a')['href'])  # getting the image_url
 
-                # Looking for Unwanted Tags
-                for p in content:
-                    unwanted_list.append(p.find('div'))
+                    # Looking for Unwanted Tags
+                    for p in content:
+                        unwanted_list.append(p.find('div'))
 
-                # Looking for Content Tags
-                for p in content:
-                    pr = p.find_all('p')
-                    unwanted = p.find('div')
-                    unwanted.extract()
-                    for pr in pr:
-                        content_list.append(pr)
+                    # Looking for Content Tags
+                    for p in content:
+                        pr = p.find_all('p')
+                        unwanted = p.find('div')
+                        unwanted.extract()
+                        for pr in pr:
+                            content_list.append(pr)
 
-            # print(content_list)
-            except:
-                desc_list = None
-                img_list = None
-                unwanted_list = None
+                # print(content_list)
+                except:
+                    desc_list = None
+                    img_list = None
+                    unwanted_list = None
 
-            # Getting the finished looped post details data
-            try:
-                discription = ' '.join([str(x) for x in desc_list])
-                image_url = ' '.join([str(x) for x in img_list])
-                content = '\n'.join([str(x) for x in content_list])
-                # content = '\n'.join([str(x) for x in content])
+                # Getting the finished looped post details data
+                try:
+                    discription = ' '.join([str(x) for x in desc_list])
+                    image_url = ' '.join([str(x) for x in img_list])
+                    content = '\n'.join([str(x) for x in content_list])
+                    # content = '\n'.join([str(x) for x in content])
 
-                qs = Post.objects.filter(title=title)
-                if not qs.exists():
-                    Post.objects.create(
-                        title=title, 
-                        description=discription,
-                        content=content, 
-                        author=USER_1,
-                        image_url=image_url,
-                        scraped=True
-                        )
-                    print('%s added' % (title,))
-                else:
-                    print('%s already exists' % (title,))
-            except:
-                print('%s can`t be posted' % (title,))
+                    qs = Post.objects.filter(title=title)
+                    if not qs.exists():
+                        Post.objects.create(
+                            title=title, 
+                            description=discription,
+                            content=content, 
+                            author=USER_1,
+                            image_url=image_url,
+                            scraped=True
+                            )
+                        print('%s added' % (title,))
+                    else:
+                        print('%s already exists' % (title,))
+                except:
+                    print('%s can`t be posted' % (title,))
+            else:
+                print('%s => exists' % (title,))
         self.stdout.write( 'Scraped Sucessful ✅' )
